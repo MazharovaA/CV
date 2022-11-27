7460323a567f79a11a15299bad09c16d7859f52c
MazharovaA <116886909+MazharovaA@users.noreply.github.com>
2022-11-27 22:57:26 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	$('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptivHeight:true,
+        speed:1000,
+        infinite:false,
+        swipe:true,
+        TouchMove:true,
+        Waitforanimate:false,
+        responsive:[
+            {
+                breakpoint:968,
+                settings:{
+                    arrows:false,
+                }
+            }
+
+        ]
+    })
+    /*$('figma').click(function(event){
+        $('slider').slick('goTo',0);
+
+    })*/
+    
+    $('slider2').slick({
+        arrows:false,
+        dots:false,
+        adaptivHeight:true,
+        speed:1000,
+        infinite:false,
+        swipe:true,
+        TouchMove:true,
+        Waitforanimate:false,
+        responsive:[
+            {
+                breakpoint:968,
+                settings:{
+                    arrows:false,
+                }
+            }
+
+        ]
+    })
+
+    $('slider3').slick({
+        dots:false,
+    })
+})
+
